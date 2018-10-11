c9446aedd2129e45dd43f7716503296db904fb5c
Michal <sunday1601@gmail.com>
2018-10-11 21:27:16 +0100

    Create gradle wrapper version 4.10.2@

--- src/main/java/org/webapp/test/Ch1Servlet.java
+++ src/main/java/org/webapp/test/Ch1Servlet.java
@@ -1,11 +1,13 @@
 package org.webapp.test;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet
 public class Ch1Servlet extends HttpServlet {
 
     @Override
